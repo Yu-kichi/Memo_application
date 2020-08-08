f34e5cd334b20441bcd009ea0106e3473f8ebf8b
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2020-08-08 18:11:32 +0900

    Modify Pull request

--- Gemfile
+++ Gemfile
@@ -4,8 +4,12 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'dotenv'
+gem 'pg'
 gem 'redcarpet'
+gem 'rubocop'
+gem 'rubocop-performance'
+gem 'rubocop-rails'
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'pg'
 # gem "rails"
--- main.rb
+++ main.rb
@@ -1,103 +1,106 @@
 # frozen_string_literal: true
 
-require "sinatra"
-require "sinatra/reloader"
-require "pg"
-# ファイルの読み込みと書き込み
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+require 'dotenv'
+
+Dotenv.load
+
 class MemoDatebase
   def self.open
-    settings = { dbname: "memo2" }
+    settings = { dbname: ENV['DB_NAME'], user: ENV['LOGIN_NAME'], password: ENV['LOGIN_PASSWORD'] }
     conn = PG.connect(settings)
     yield conn
-    rescue PG::Error => e
-      puts e.message
+  rescue PG::Error => e
+    puts e.message
   ensure
-    conn.close if conn
+    conn&.close
   end
 end
 
-class MemoDbQuery
+class MemoDatebaseQuery
   def self.find_all
     MemoDatebase.open do |conn|
-      conn.exec("SELECT * FROM Memo ORDER BY updated_at DESC")
+      conn.exec('SELECT * FROM memo ORDER BY updated_at DESC')
     end
   end
 
-  def self.find_id(id)
+  def self.find_by_id(id)
     MemoDatebase.open do |conn|
-      conn.prepare("find", "SELECT * FROM memo WHERE memo_id=$1;")
-      conn.exec_prepared("find", [id])
+      conn.prepare('find', 'SELECT * FROM memo WHERE memo_id=$1;')
+      conn.exec_prepared('find', [id])
     end
   end
 
-  def self.create(title, body, time)
+  def self.create(title, body)
+    time = Time.now
     MemoDatebase.open do |conn|
-      conn.prepare("new", "INSERT INTO Memo (memo_id,memo_title,memo_body,created_at,updated_at)values (DEFAULT, $1, $2,$3, $4)")
-      conn.exec_prepared("new", [ title, body, time, time])
+      conn.prepare('new', 'INSERT INTO memo (memo_id,memo_title,memo_body,created_at,updated_at)values (DEFAULT, $1, $2, $3, $4)')
+      conn.exec_prepared('new', [title, body, time, time])
     end
   end
 
-  def self.edit(title, body, time, id)
+  def self.update(title, body, id)
+    time = Time.now
     MemoDatebase.open do |conn|
-      conn.prepare("update", "UPDATE Memo SET memo_title = $1, memo_body = $2, updated_at = $3 WHERE memo_id = $4;")
-      conn.exec_prepared("update", [title, body, time, id])
+      conn.prepare('update', 'UPDATE memo SET memo_title = $1, memo_body = $2, updated_at = $3 WHERE memo_id = $4;')
+      conn.exec_prepared('update', [title, body, time, id])
     end
   end
 
   def self.delete(id)
     MemoDatebase.open do |conn|
-      conn.prepare("delete", "DELETE FROM memo WHERE memo_id = $1;")
-      conn.exec_prepared("delete", [id])
+      conn.prepare('delete', 'DELETE FROM memo WHERE memo_id = $1;')
+      conn.exec_prepared('delete', [id])
     end
   end
 end
 
 helpers do
   def convert_to_br(body)
-    body.gsub(/\r/, "<br>")
+    body.gsub(/\r\n|\n|\r/, '<br>')
   end
 end
 
-get "/" do
-  redirect "/memos"
+get '/' do
+  redirect '/memos'
 end
 
-get "/memos" do
-  @results= MemoDbQuery.find_all
+get '/memos' do
+  @memos = MemoDatebaseQuery.find_all
   erb :index
 end
 
-get "/memos/new" do
+get '/memos/new' do
   erb :memo_new
 end
 
-get "/memos/:id" do |id|
+get '/memos/:id' do |id|
   @id = id
-  @result= MemoDbQuery.find_id(@id)
-  erb :memo_show
+  @result = MemoDatebaseQuery.find_by_id(@id)
+  @result.ntuples.positive? ? erb(:memo_show) : pass
 end
 
-post "/memos" do
-  time = Time.now
-  MemoDbQuery.create(params[:title], params[:body], time)
-  redirect "/memos"
+post '/memos' do
+  MemoDatebaseQuery.create(params[:title], params[:body])
+  redirect '/memos'
 end
 
-get "/memos/:id/edit" do |id|
+get '/memos/:id/edit' do |id|
   @id = id
-  @result= MemoDbQuery.find_id(@id)
-  erb :memo_edit
+  @result = MemoDatebaseQuery.find_by_id(@id)
+  @result.ntuples.positive? ? erb(:memo_edit) : pass
 end
 
-patch "/memos/:id" do |id|
+patch '/memos/:id' do |id|
   @id = id
-  time = Time.now
-  MemoDbQuery.edit(params[:title], params[:body], time, @id)
-  redirect "/memos"
+  MemoDatebaseQuery.update(params[:title], params[:body], @id)
+  redirect '/memos'
 end
 
-delete "/memos/:id" do |id|
+delete '/memos/:id' do |id|
   @id = id
-  MemoDbQuery.delete(@id)
-  redirect "/memos"
+  MemoDatebaseQuery.delete(@id)
+  redirect '/memos'
 end
